69ffeea30df57430ad1d3ee27ccb7d23ea8de88b
mavisphung <huypc2410@gmail.com>
2020-07-20 15:27:17 +0700

    4th: Add the exception handler

--- src/huy/util/MathUtil.java
+++ src/huy/util/MathUtil.java
@@ -15,6 +15,8 @@ public class MathUtil {
     //hàm tính n! = 1*2*3*4....
     //Vì n! tăng rất nhanh, sẽ tràn int sớm (tối da 2 tỷ 1) nên mình xài long để an toàn hơn.
     public static long computeFactorial(int n) {
+        //nếu người dùng đưa cà chớn, ta sẽ ném ra ngoại lệ
+        if (n < 0 || n > 15) throw new IllegalArgumentException("Invalid argument: n must be >= 0 and <= 15");
         long result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
